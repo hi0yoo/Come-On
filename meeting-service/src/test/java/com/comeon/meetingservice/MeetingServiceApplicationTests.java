61e403fbccf7e99ed3ae6851a57dd0e17b757ac6
cares0 <cares00000@gmail.com>
2022-08-08 12:05:33 +0900

    chore: Meeting Service 생성 및 초기 연동
    Eureka Client 등록 및 Config Server 연동
    
    Related to: #CO-22, #CO-21, #CO-25

---
+++ meeting-service/src/test/java/com/comeon/meetingservice/MeetingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comeon.meetingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
