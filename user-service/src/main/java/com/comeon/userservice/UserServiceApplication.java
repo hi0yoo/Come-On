1d8d35052d3de9d745cb809b24d247e137833526
YooHayoung <yoo971202@naver.com>
2022-09-01 00:30:23 +0900

    chore : feign client 의존성 추가
    feign client 의존성 추가
    FeignClient 사용
    
    Related to : #CO-119

--- user-service/src/main/java/com/comeon/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/comeon/userservice/UserServiceApplication.java
@@ -3,8 +3,10 @@ package com.comeon.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableFeignClients
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableDiscoveryClient
