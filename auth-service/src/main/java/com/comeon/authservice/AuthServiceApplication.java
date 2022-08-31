ff283b44e1986c6e84bbffab442690750dbbff2b
cares0 <cares00000@gmail.com>
2022-08-31 14:28:06 +0900

    chore: auth-service 도커 파일 생성
    - auth-service 도커 파일 생성

--- auth-service/src/main/java/com/comeon/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/comeon/authservice/AuthServiceApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableFeignClients
-@EnableJpaAuditing
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AuthServiceApplication {
