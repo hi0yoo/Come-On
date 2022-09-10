06a0568a2a2902552b28e32395c704ac87003e48
cares0 <cares00000@gmail.com>
2022-09-10 22:25:30 +0900

    chore: feign logger 설정
    - feign client 통신 시 로그 출력하도록 설정

---
+++ meeting-service/src/main/java/com/comeon/meetingservice/web/common/feign/FeignConfig.java
@@ -0,0 +1,14 @@
+package com.comeon.meetingservice.web.common.feign;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+}
