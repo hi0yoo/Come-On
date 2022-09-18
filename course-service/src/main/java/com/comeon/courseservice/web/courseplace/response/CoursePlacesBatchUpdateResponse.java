6fa3deb87a0b85dc9640bcba1d04dc9b05294150
YooHayoung <yoo971202@naver.com>
2022-09-18 16:05:58 +0900

    feat : 코스 장소 리스트 변경시 응답 필드 추가
    코스 장소 리스트 변경 성공시, 코스 식별값, 코스 상태를 응답에 추가
    
    Related to : #CO-113

--- course-service/src/main/java/com/comeon/courseservice/web/courseplace/response/CoursePlacesBatchUpdateResponse.java
+++ course-service/src/main/java/com/comeon/courseservice/web/courseplace/response/CoursePlacesBatchUpdateResponse.java
@@ -1,5 +1,6 @@
 package com.comeon.courseservice.web.courseplace.response;
 
+import com.comeon.courseservice.domain.course.entity.CourseStatus;
 import lombok.Getter;
 
 @Getter
@@ -7,13 +8,20 @@ public class CoursePlacesBatchUpdateResponse {
 
     public static final String SUCCESS_MESSAGE = "코스 장소 리스트 업데이트가 성공적으로 완료되었습니다.";
 
+    private Long courseId;
+    private CourseStatus courseStatus;
+
     private String message;
 
-    public CoursePlacesBatchUpdateResponse() {
+    public CoursePlacesBatchUpdateResponse(Long courseId, CourseStatus courseStatus) {
+        this.courseId = courseId;
+        this.courseStatus = courseStatus;
         this.message = SUCCESS_MESSAGE;
     }
 
-    public CoursePlacesBatchUpdateResponse(String message) {
+    public CoursePlacesBatchUpdateResponse(Long courseId, CourseStatus courseStatus, String message) {
+        this.courseId = courseId;
+        this.courseStatus = courseStatus;
         this.message = message;
     }
 }
